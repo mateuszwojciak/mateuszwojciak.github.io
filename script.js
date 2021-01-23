ecda0f936aa49bdd6c26a745b852eb09e267c8f0
Mateusz Wójciak <matwojciak@gmail.com>
2021-01-23 14:44:25 +0100

    Change for localhost

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   const apiRoot = 'http://shrouded-waters-25989.herokuapp.com/v1/task/';
+   const apiRoot = 'http://localhost:8080/v1/task/';
    const trelloApiRoot = 'http://localhost:8080/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
